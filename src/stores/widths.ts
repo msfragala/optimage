cc16026f3f09c07410102b026a521d06744ab29a
Mitchell Fragala <ms.fragala@gmail.com>
2022-10-11 00:36:21 -0400

    Remember last widths settings used

--- src/stores/widths.ts
+++ src/stores/widths.ts
@@ -1,14 +1,30 @@
 import { writable } from 'svelte/store';
 
-const store = writable([400, 800, 1200]);
+let initialValue = [400, 800, 1200];
+
+const storedValue = JSON.parse(localStorage.getItem('widths'));
+
+if (Array.isArray(storedValue) && storedValue.every(n => Number.isInteger(n))) {
+  initialValue = storedValue;
+}
+
+const store = writable(initialValue);
 
 export const widths$ = {
   subscribe: store.subscribe,
   add(value: number) {
-    store.update(state => state.concat(value).sort((a, b) => a - b));
+    store.update(state => {
+      const next = state.concat(value).sort((a, b) => a - b);
+      localStorage.setItem('widths', JSON.stringify(next));
+      return next;
+    });
   },
   remove(value: number) {
-    store.update(state => state.filter(x => x !== value));
+    store.update(state => {
+      const next = state.filter(x => x !== value);
+      localStorage.setItem('widths', JSON.stringify(next));
+      return next;
+    });
   },
   reset() {
     store.set([]);
